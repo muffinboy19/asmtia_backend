69ff47db8842bbcd5bc2471c718718cdddde7605
Om Buddhadev <buddhadevom@gmail.com>
2024-02-24 12:39:52 +0530

    football and cricket

---
+++ models/cricketresults.chema.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose
+//this is the second type of model for fixtures
+const cricketSchema = new Schema({
+    ClgImg1:{
+        type: Schema.Types.String,
+        ref: "ClgImg1",
+        default: "https://t3.ftcdn.net/jpg/02/78/42/76/360_F_278427683_zeS9ihPAO61QhHqdU1fOaPk2UClfgPcW.jpg",
+    },
+    ClgImg2:{
+        type: Schema.Types.String,
+        ref: "ClgImg2",
+        default: "https://t3.ftcdn.net/jpg/02/78/42/76/360_F_278427683_zeS9ihPAO61QhHqdU1fOaPk2UClfgPcW.jpg",
+    },
+
+    ClgName1:{
+        type: Schema.Types.String,
+        ref: "ClgName1",
+        default: "IIIT-A",
+    },
+    ClgName2:{
+        type: Schema.Types.String,
+        ref: "ClgName2",
+        default: "IIIT-A",
+    },
+    Date:{
+        type: Schema.Types.String,
+        ref: "Date",
+        default: "12/12/2021",
+    },
+    GroupStage:{
+        type: Schema.Types.String,
+        ref: "GroupStage",
+        default: "Semi Final",
+    },
+    MatchName:{
+        type: Schema.Types.String,
+        ref: "MatchName",
+        default: "Relay - 500m",
+    },
+    Over1:{
+        type: Schema.Types.String,
+        ref: "Over1",
+        default: "20",
+    }, 
+    Over2:{
+        type: Schema.Types.String,
+        ref: "Over2",
+        default: "20",
+    },
+    Score1:{
+        type: Schema.Types.String,
+        ref: "Score1",
+        default: "200/10",
+    },
+    Score2:{
+        type: Schema.Types.String,
+        ref: "Score2",
+        default: "180/2",
+    },
+})
+
+const CricketResults = model("CricketResults",cricketSchema );
+export default CricketResults;
---
+++ models/footballresults.schema.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose
+//this is the second type of model for fixtures
+const footballSchema = new Schema({
+    ClgImg1:{
+        type: Schema.Types.String,
+        ref: "ClgImg1",
+        default: "https://t3.ftcdn.net/jpg/02/78/42/76/360_F_278427683_zeS9ihPAO61QhHqdU1fOaPk2UClfgPcW.jpg",
+    },
+    ClgImg2:{
+        type: Schema.Types.String,
+        ref: "ClgImg2",
+        default: "https://t3.ftcdn.net/jpg/02/78/42/76/360_F_278427683_zeS9ihPAO61QhHqdU1fOaPk2UClfgPcW.jpg",
+    },
+
+    ClgName1:{
+        type: Schema.Types.String,
+        ref: "ClgName1",
+        default: "IIIT-A",
+    },
+    ClgName2:{
+        type: Schema.Types.String,
+        ref: "ClgName2",
+        default: "IIIT-A",
+    },
+    Date:{
+        type: Schema.Types.String,
+        ref: "Date",
+        default: "12/12/2021",
+    },
+    GroupStage:{
+        type: Schema.Types.String,
+        ref: "GroupStage",
+        default: "Semi Final",
+    },
+    MatchName:{
+        type: Schema.Types.String,
+        ref: "MatchName",
+        default: "Relay - 500m",
+    },
+    Point:{
+        type: Schema.Types.String,
+        ref: "Point",
+        default: "4 - 6",
+    },
+})
+
+const FootballResults = model("FootballResults",footballSchema );
+export default FootballResults;
