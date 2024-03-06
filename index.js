0491365908b8c1b82a1f0e7cc7472f9431529497
Vihaan-19 <vihaanmarathey10@gmail.com>
2024-03-07 01:58:24 +0530

    status monitor

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ import dotenv from "dotenv";
 
 import { connectDB } from "./config/db.config.js";
 import appRouter from "./routes/api.js";
+import statusMonitor from "express-status-monitor";
 
 dotenv.config();
-
+// const statusMonitor = require('express-status-monitor');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(statusMonitor());
 // Routes
 
 app.use("/api", appRouter);
