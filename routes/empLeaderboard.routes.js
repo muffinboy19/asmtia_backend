73ac69dc6eea5b7a4f8785f2195cf618f3897943
Vihaan-19 <vihaanmarathey10@gmail.com>
2024-03-07 23:05:29 +0530

    only heads can update leaderboards

--- routes/empLeaderboard.routes.js
+++ routes/empLeaderboard.routes.js
@@ -3,10 +3,10 @@ import {
     getLeaderboard,
     updateLeaderboardEntry,
 } from "../controllers/empLeaderboard.controllers.js";
-import { headExecMiddleware } from "../middlewares/isHeadExec.js";
+import { headMiddleware } from "../middlewares/isHeadExec.js";
 const router = express.Router();
 
 router.get("/", getLeaderboard);
-router.patch("/:id", headExecMiddleware, updateLeaderboardEntry);
+router.patch("/:id", headMiddleware, updateLeaderboardEntry);
 
 export default router;
