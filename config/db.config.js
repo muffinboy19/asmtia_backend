834b0b4325b0f1f08440a964a0da9a6215b9b832
muffinboy <87519281+muffinboy19@users.noreply.github.com>
2025-06-19 12:07:41 +0530

    this is new code

--- config/db.config.js
+++ config/db.config.js
@@ -4,21 +4,18 @@ import { createClient } from "redis";
 export const client = createClient({ url: process.env.CACHE_URI });
 
 if (process.env.NODE_ENV === "production") {
-  client.on("error", (err) => console.log("Redis Client Error", err));
-  client.on("connect", () =>
-    console.log("Connected to Redis Successfully! ✨")
-  );
+    client.on("error", (err) => console.log("Redis Client Error", err));
+    client.on("connect", () =>
+        console.log("Connected to Redis Successfully! ✨")
+    );
 
-  await client.connect();
+    await client.connect();
 }
 
-let db_url =
-  process.env.MONGO_URI ||
-  "mongodb+srv://iit2022035:DevamDesai@clusterlearn.inj4tzw.mongodb.net/xenonTest?retryWrites=true&w=majority";
-
+let db_url = process.env.MONGO_URI;
 export const connectDB = async () => {
-  await mongoose.connect(db_url, {
-    useUnifiedTopology: true,
-  });
-  console.log("\nConnected to Database Successfully! ✨");
+    await mongoose.connect(db_url, {
+        useUnifiedTopology: true,
+    });
+    console.log("\nConnected to Database Successfully! ✨");
 };
